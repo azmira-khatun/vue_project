d4dff79a5f4dd5e53e04816fbad9a068ac67b919
azmira-khatun <azmiragrn@gmail.com>
2025-12-18 12:41:29 +0600

    first commit

---
+++ pos-vue/api/resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div style="padding: 40px; font-size: 24px;">
+        ✅ Laravel + Vue is Working!
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
--- pos-vue/frontend/vite.config.js
+++ pos-vue/frontend/vite.config.js
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": "/resources/js", // Laravel folder structure অনুযায়ী
+    },
+  },
+});
